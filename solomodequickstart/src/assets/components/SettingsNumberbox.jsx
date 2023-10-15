c9b0ad94b71217acf349e3d5d6d07a3e58500201
donwung <113479299+donwung@users.noreply.github.com>
2023-10-15 15:45:34 -0700

    implemented number inputs component

--- solomodequickstart/src/assets/components/SettingsNumberbox.jsx
+++ solomodequickstart/src/assets/components/SettingsNumberbox.jsx
@@ -1,21 +1,23 @@
-const SettingsCheckbox = (props) => {
+const SettingsNumberbox = (props) => {
     const {MySettings, DefaultSettings, updateMySettings, cdSetting} = props
     return (
         <div>
             <div>
                 <label htmlFor={"input_"+cdSetting}>Set {cdSetting}</label>
                 <input
-                    type="checkbox"
+                    type="number"
+                    placeholder={"Default: "+ DefaultSettings[cdSetting]}
                     id={cdSetting}
                     onChange={(e) => { updateMySettings(e) }}
-                    checked={MySettings[cdSetting]}
+                    value={MySettings[cdSetting]}
+                    style={{width: "40px"}}
                 >
                 </input>
-                <b>Current: {MySettings[cdSetting] ? "TRUE" : "FALSE"}</b>
-                <i>Default: {DefaultSettings[cdSetting] ? "TRUE" : "FALSE"}</i>
+                <b>Current: {MySettings[cdSetting]}</b>
+                <i>Default: {DefaultSettings[cdSetting]}</i>
             </div>
         </div>
     )
 }
 
-export default SettingsCheckbox
+export default SettingsNumberbox
